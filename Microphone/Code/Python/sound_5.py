0a93023fd5504415ad7cc11f75cc3ff08562156f
GVasilyev <vasilyev.georgy@gmail.com>
2019-02-17 14:40:15 +0300

    pickling frequencies in file

--- Microphone/Code/Python/sound_5.py
+++ Microphone/Code/Python/sound_5.py
@@ -1,109 +1,183 @@
 import pyaudio
 import numpy as np
 import time
+import pickle
 
 import peakdetect
 import customfirwin_ as fir
 import savgol_filter as sgf
 
 
-# Sound stream params
-CHUNK = 1024  # samples per buffer
-FORMAT = pyaudio.paInt16  # bytes per sample
-CHANNELS = 1  # number of channels for micro
-RATE = 8000 # Sampling frequency; 44100 Hz is default
-
-pa = pyaudio.PyAudio()
-
-stream = pa.open(
-    format=FORMAT,
-    channels=CHANNELS,
-    rate=RATE,
-    input=True,
-    output=False,
-    frames_per_buffer=CHUNK#,
-    #input_device_index=2
-)
-
-nyq = RATE//2
-ts = 1/RATE # Ts is period
-M = 256  # number of taps in filter
-fc = 0.4756  # from 100 to 4096 bandpass
-
-ham_lp = fir.build_filter(M, fc, window=fir.hamming)
-black_lp = fir.build_filter(M, fc, window=fir.blackman)
-shift = (np.cos(2 * np.pi * 0.5 * np.arange(M + 1)))
-ham_hp = ham_lp * shift
-black_hp = black_lp * shift
-
-while True:
-
-    # Get binary data
-    data = stream.read(CHUNK, exception_on_overflow = False)
-    time.sleep(0.005)
-    data_int = np.frombuffer(data, dtype=np.int16) # Convert data from buffer to int
-    data_np = [(i/(2**14)) for i in data_int] # 16 bits = 2^16; first bit for sign; 2^15/2: from -(2^14) to 2^14
-    f_ham = np.convolve(data_np, ham_hp)
-    f_black = np.convolve(data_np, black_hp)
-
-    # FFT
-    y_fft  = np.fft.fft(data_np)
-    y_fft_ = np.fft.fft(fir.hamming(len(f_black))[:-1]*(f_black))
-    gain_coeff = 10.0 # Gain for freqs values    1.5
-    spec_data = np.abs(y_fft_[0:CHUNK] / CHUNK * gain_coeff)
-
-    spec_data = sgf.savgolfilt(spec_data, 5, 7, 1)
-    maxs, mins = peakdetect.peakdet(spec_data, 0.085) # 0.035
+def get_peaks(data, delta=0.085, damping=True, RATE=8000, CHUNK=1024):
+    '''
+    :param data: spectrum data
+    :param delta: see peakdetect.py
+    :param damping: neglect a point if amplitude is lower than threshold
+    :return: peaks as array of points(freq;amplitude)
+    '''
+    maxs, mins = peakdetect.peakdet(data, delta)  # 0.035
 
     # Convert taps number in frequencies
     if (len(maxs) > 0):
         for i in range(len(maxs)):
-            maxs[i][0] *= RATE/CHUNK*0.8
-            #maxs[0:len(maxs)][0] *= RATE/CHUNK
-        maxs = maxs[:len(maxs) // 2] # Get first part of vector
-    
-    #Delete peaks lower then threshold
-    flag = True
-    while(flag):
-        flag = False
-        for i in range (len(maxs)):
-            if (maxs[i][1]<0.7): # 0.5
-                maxs = np.delete(maxs,i,0)
-                flag = True
-                break
-    
-    #Print peaks 
-    #if (len(maxs)>0):
-        #print(list(maxs))
+            maxs[i][0] *= RATE / CHUNK * 0.8
+            # maxs[0:len(maxs)][0] *= RATE/CHUNK
+        maxs = maxs[:len(maxs) // 2]  # Get first part of vector
 
+    # Delete peaks lower then threshold
+    while (damping):
+        damping = False
+        for i in range(len(maxs)):
+            if (maxs[i][1] < 0.7):  # 0.5
+                maxs = np.delete(maxs, i, 0)
+                damping = True
+                break
+
+    # Print peaks
+    # if (len(maxs)>0):
+    # print(list(maxs))
+
+    return maxs
+
+def red_blue(data):
+    '''
+    simple 'red'/'blue' word detector for a specific voice
+    :param data: frequency peak points
+    :return: recognized word as string
+    '''
     red_check0 = False
     red_check1 = False
     blue_check0 = False
     blue_check1 = False
 
-    for i in range(len(maxs)):
+    for i in range(len(data)):
         #Red check
-        if((maxs[i][0] > 510) and (maxs[i][0] < 640)
-           and (maxs[i][1] > 1.7)):
+        if((data[i][0] > 510) and (data[i][0] < 640)
+           and (data[i][1] > 1.7)):
             red_check0 = True
-        if(((maxs[i][0] > 1500 and maxs[i][0] < 1550) 
-            or (maxs[i][0] > 1750 and maxs[i][0] < 1850))
-            and maxs[i][1] > 1.2):
+        if(((data[i][0] > 1500 and data[i][0] < 1550)
+            or (data[i][0] > 1750 and data[i][0] < 1850))
+            and data[i][1] > 1.2):
             red_check1 = True
         #Blue check
-        if((maxs[i][0] > 250) and (maxs[i][0] < 450) 
-           and (maxs[i][1] > 1.2)):
+        if((data[i][0] > 250) and (data[i][0] < 450)
+           and (data[i][1] > 1.2)):
             blue_check0 = True
-        if((maxs[i][0] > 1150) and (maxs[i][0] < 1450) 
-           and (maxs[i][1] > 1.2)): # 1.0
+        if((data[i][0] > 1150) and (data[i][0] < 1450)
+           and (data[i][1] > 1.2)): # 1.0
             blue_check1 = True
 
     if (red_check0 and red_check1):
-        print('RED')
-        print('')
+        return 'red'
     elif (blue_check0 and blue_check1):
-        print('BLUE')
-        print('')
+        return 'blue'
+    elif (len(data) > 0):
+        return 'something'
+    else:
+        return 'none'
 
-    if (len(maxs) > 0 and not(red_check0 and red_check1) and not(blue_check0 and blue_check1)):
+def simple_sound_ckeck(data):
+    color = red_blue(data)
+
+    if (color == 'something'):
         print('I hear something')
+    elif (color != 'none'):
+        print(color)
+
+def write_freqs(spec_data, fname, num):
+    '''
+    :param spec_data: frequency spectrum list
+    :param fname: file name for sound samples
+    :param num: number of saved samples
+    :return: num
+    '''
+    print(num)
+    add_to_file = True
+    file = open(fname, 'rb')
+    for i in range(num):
+        sav_data = pickle.load(file)
+        corr = np.corrcoef(spec_data, sav_data)
+
+        if corr[0][1] > 0.8:  # 0.6 is optimal
+            print(corr[0][1])
+            add_to_file = False
+
+    file.close()
+
+    if (add_to_file):
+        file = open(fname, 'ab')
+        pickle.dump(spec_data, file)
+        file.close()
+        num += 1
+        print('new value has been added')
+
+    return num
+
+def main():
+
+    # Sound stream params
+    CHUNK = 1024  # samples per buffer
+    FORMAT = pyaudio.paInt16  # bytes per sample
+    CHANNELS = 1  # number of channels for micro
+    RATE = 8000 # Sampling frequency; 44100 Hz is default
+
+    pa = pyaudio.PyAudio()
+
+    stream = pa.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=RATE,
+        input=True,
+        output=False,
+        frames_per_buffer=CHUNK#,
+        #input_device_index=2
+    )
+
+    nyq = RATE//2
+    ts = 1/RATE # Ts is period
+    M = 256  # number of taps in filter
+    fc = 0.4756  # from 100 to 4096 bandpass
+
+    ham_lp = fir.build_filter(M, fc, window=fir.hamming)
+    black_lp = fir.build_filter(M, fc, window=fir.blackman)
+    shift = (np.cos(2 * np.pi * 0.5 * np.arange(M + 1)))
+    ham_hp = ham_lp * shift
+    black_hp = black_lp * shift
+
+    print('START SOUND SIGNAL PROCESSING')
+
+    fname = 'freqs.txt'
+    file = open(fname, 'wb+')
+    sav_data = np.random.rand(1,1024)
+    pickle.dump(sav_data, file)
+    file.close()
+
+    counter = 1
+
+    while True:
+
+        # Get binary data
+        data = stream.read(CHUNK) # ,exception_on_overflow = False; add this arg for RASPBERRY
+        time.sleep(0.1)
+        data_int = np.frombuffer(data, dtype=np.int16) # Convert data from buffer to int
+        data_np = [(i/(2**14)) for i in data_int] # 16 bits = 2^16; first bit for sign; 2^15/2: from -(2^14) to 2^14
+        f_ham = np.convolve(data_np, ham_hp)
+        f_black = np.convolve(data_np, black_hp)
+
+        # FFT
+        y_fft  = np.fft.fft(data_np)
+        y_fft_ = np.fft.fft(fir.hamming(len(f_black))[:-1]*(f_black))
+        gain_coeff = 10.0 # Gain for freqs values    10.0
+        spec_data = np.abs(y_fft_[0:CHUNK] / CHUNK * gain_coeff)
+
+        spec_data = sgf.savgolfilt(spec_data, 5, 7, 1)
+
+        #print(spec_data)
+        maxs = get_peaks(spec_data)
+        #simple_sound_ckeck(maxs)
+
+        if len(maxs) > 0:
+            counter = write_freqs(spec_data, fname, counter)
+
+if __name__ == '__main__':
+    main()
